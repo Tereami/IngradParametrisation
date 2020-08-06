525d6262dedd0a135baec7d9f6a227f0d879997a
Tereami <undress.my.future@gmail.com>
2020-08-06 18:02:05 +0300

    Добавьте файлы проекта.

---
+++ IngradParametrisation/Cmd.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB; //для работы с элементами модели Revit
+using Autodesk.Revit.UI; //для работы с элементами интерфейса
+
+namespace IngradParametrisation
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class Cmd : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            string paramNameGroupConstr = "INGD_Группа конструкции";
+            string floorNumberParamName = "INGD_Номер этажа";
+            int floorTextPosition = 0;
+
+            App.assemblyPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            string markParamName = "INGD_Марка";
+            string widthParamName = "INGD_Ширина";
+            string lengthParamName = "INGD_Длина";
+            string heigthParamName = "INGD_Высота";
+
+            Dictionary<string, string> marksBase = new Dictionary<string, string>();
+            string assemblyFolder = System.IO.Path.GetDirectoryName(App.assemblyPath);
+            string txtFile = System.IO.Path.Combine(assemblyFolder, "config.txt");
+            if(!System.IO.File.Exists(txtFile))
+            {
+                message = "Не найден файл " + txtFile;
+                return Result.Failed;
+            }
+
+            string[] data = System.IO.File.ReadAllLines(txtFile);
+            foreach(string s in data)
+            {
+                string[] line = s.Split(';');
+                marksBase.Add(line[0], line[1]);
+            }
+
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            List<Element> allElems = new List<Element>();
+
+            List<Wall> walls = new FilteredElementCollector(doc)
+                .OfClass(typeof(Wall))
+                .Cast<Wall>()
+                .ToList();
+
+            List<Floor> floors = new FilteredElementCollector(doc)
+                .OfClass(typeof(Floor))
+                .Cast<Floor>()
+                .ToList();
+
+            allElems.AddRange(walls);
+            allElems.AddRange(floors);
+            allElems.AddRange(new FilteredElementCollector(doc).OfClass(typeof(Autodesk.Revit.DB.Architecture.Stairs)));
+            allElems.AddRange(new FilteredElementCollector(doc).OfClass(typeof(FamilyInstance)).OfCategory(BuiltInCategory.OST_StructuralColumns));
+            allElems.AddRange(new FilteredElementCollector(doc).OfClass(typeof(FamilyInstance)).OfCategory(BuiltInCategory.OST_StructuralFraming));
+            allElems.AddRange(new FilteredElementCollector(doc).OfClass(typeof(FamilyInstance)).OfCategory(BuiltInCategory.OST_StructuralFoundation));
+
+            List<FamilyInstance> genericModels = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilyInstance))
+                .OfCategory(BuiltInCategory.OST_GenericModel)
+                .Cast<FamilyInstance>()
+                .Where(i => !i.Symbol.FamilyName.StartsWith("22"))
+                .ToList();
+            allElems.AddRange(genericModels);
+
+            List<FamilyInstance> windows = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilyInstance))
+                .Cast<FamilyInstance>()
+                .Where(i => i.Symbol.FamilyName.StartsWith("23"))
+                .ToList();
+            allElems.AddRange(windows);
+
+            List<RoofBase> roofs = new FilteredElementCollector(doc)
+                .OfClass(typeof(Autodesk.Revit.DB.RoofBase))
+                .Cast<RoofBase>()
+                .ToList();
+
+            int count = 0;
+
+
+
+            using (Transaction t = new Transaction(doc))
+            {
+                t.Start("INGD Параметризация");
+
+                foreach (Element elem in allElems)
+                {
+                    Parameter markParam = elem.get_Parameter(BuiltInParameter.ALL_MODEL_MARK);
+                    if (markParam == null) continue;
+                    if (markParam.HasValue)
+                    {
+                        string mark = markParam.AsString();
+
+                        Parameter ingdMarkParam = elem.LookupParameter(markParamName);
+                        if (ingdMarkParam != null)
+                        {
+                            ingdMarkParam.Set(mark);
+                        }
+
+                        string[] splitmark = mark.Split('-');
+                        if (splitmark.Length > 1)
+                        {
+                            string markPrefix = splitmark[0];
+                            if (!marksBase.ContainsKey(markPrefix))
+                            {
+                                TaskDialog.Show("Ошибка", "Недопустимый префикс марки " + markPrefix + " у элемента id " + elem.Id.IntegerValue.ToString());
+                                return Result.Failed;
+                            }
+                            string group = marksBase[markPrefix];
+                            Parameter groupParam = elem.LookupParameter(paramNameGroupConstr);
+                            if (groupParam != null)
+                            {
+                                groupParam.Set(group);
+                            }
+                        }
+                    }
+
+                    //заполняю номер этажа
+                    Level baseLevel = LevelUtils.GetLevelOfElement(elem, doc);
+                    if (baseLevel != null)
+                    {
+                        string floorNumber = LevelUtils.GetFloorNumberByLevel(baseLevel, floorTextPosition);
+                        Parameter ingdFloor = elem.LookupParameter(floorNumberParamName);
+                        if (ingdFloor == null) continue;
+                        ingdFloor.Set(floorNumber);
+                    }
+
+                    count++;
+                }
+
+                foreach (RoofBase roof in roofs)
+                {
+                    string group = "Бетонная подготовка";
+                    Parameter groupParam = roof.LookupParameter(paramNameGroupConstr);
+                    if (groupParam != null)
+                    {
+                        groupParam.Set(group);
+                    }
+                }
+
+                foreach (Wall w in walls)
+                {
+                    Level baseLevel = doc.GetElement(w.LevelId) as Level;
+                    string floorNumber = LevelUtils.GetFloorNumberByLevel(baseLevel, floorTextPosition);
+                    Parameter ingdFloor = w.LookupParameter(floorNumberParamName);
+                    if (ingdFloor == null) continue;
+                    ingdFloor.Set(floorNumber);
+
+                    double width = w.Width;
+                    Parameter ingdWidth = w.LookupParameter(widthParamName);
+                    if (ingdWidth == null) continue;
+                    ingdWidth.Set(width);
+
+                    double length = w.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble();
+                    Parameter ingdLength = w.LookupParameter(lengthParamName);
+                    if (ingdLength == null) continue;
+                    ingdLength.Set(length);
+
+                    List<Solid> solids = GeometryUtils.GetSolidsFromElement(w);
+                    XYZ[] maxminZ = GeometryUtils.GetMaxMinHeightPoints(solids);
+                    double heigth = maxminZ[0].Z - maxminZ[1].Z;
+                    Parameter ingdHeigth = w.LookupParameter(heigthParamName);
+                    if (ingdHeigth == null) continue;
+                    ingdHeigth.Set(heigth);
+                }
+
+                foreach (Floor f in floors)
+                {
+                    Level baseLevel = doc.GetElement(f.LevelId) as Level;
+                    string floorNumber = LevelUtils.GetFloorNumberByLevel(baseLevel, floorTextPosition);
+                    Parameter ingdFloor = f.LookupParameter(floorNumberParamName);
+                    if (ingdFloor == null) continue;
+                    ingdFloor.Set(floorNumber);
+
+                    FloorType ft = f.FloorType;
+
+                    double heigth = ft.get_Parameter(BuiltInParameter.FLOOR_ATTR_DEFAULT_THICKNESS_PARAM).AsDouble();
+                    Parameter ingdHeigth = f.LookupParameter(heigthParamName);
+                    if (ingdHeigth == null) continue;
+                    ingdHeigth.Set(heigth);
+                }
+
+                t.Commit();
+            }
+
+            TaskDialog.Show("Отчет", "Обработано элементов: " + count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ IngradParametrisation/LevelUtils.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace IngradParametrisation
+{
+    public static class LevelUtils
+    {
+        public static string GetFloorNumberByLevel(Level lev, int floorTextPosition)
+        {
+            string levname = lev.Name;
+            string[] splitname = levname.Split(' ');
+            if (splitname.Length < 2) throw new Exception("Некорректное имя уровня: " + levname);
+            string floorNumber = splitname[floorTextPosition];
+            return floorNumber;
+        }
+        public static Level GetLevelOfElement(Element elem, Document doc)
+        {
+            ElementId levId = elem.LevelId;
+            if (levId == ElementId.InvalidElementId)
+            {
+                Parameter levParam = elem.get_Parameter(BuiltInParameter.SCHEDULE_LEVEL_PARAM);
+                if (levParam != null)
+                    levId = levParam.AsElementId();
+            }
+
+            if (levId == ElementId.InvalidElementId)
+            {
+                Parameter levParam = elem.get_Parameter(BuiltInParameter.FAMILY_LEVEL_PARAM);
+                if (levParam != null)
+                    levId = levParam.AsElementId();
+            }
+
+            if (levId == ElementId.InvalidElementId)
+            {
+                Parameter levParam = elem.get_Parameter(BuiltInParameter.INSTANCE_REFERENCE_LEVEL_PARAM);
+                if (levParam != null)
+                    levId = levParam.AsElementId();
+            }
+
+            if (levId == ElementId.InvalidElementId)
+            {
+                List<Solid> solids = GeometryUtils.GetSolidsFromElement(elem);
+                if (solids.Count == 0) return null;
+                XYZ[] maxmin = GeometryUtils.GetMaxMinHeightPoints(solids);
+                XYZ minPoint = maxmin[1];
+                levId = GetNearestLevel(minPoint, doc);
+            }
+
+            if (levId == ElementId.InvalidElementId)
+                throw new Exception("Не удалось получить уровень у элемента " + elem.Id.IntegerValue.ToString());
+
+            Level lev = doc.GetElement(levId) as Level;
+            return lev;
+        }
+
+        public static ElementId GetNearestLevel(XYZ point, Document doc)
+        {
+            BasePoint projectBasePoint = new FilteredElementCollector(doc)
+                .OfClass(typeof(BasePoint))
+                .WhereElementIsNotElementType()
+                .Cast<BasePoint>()
+                .Where(i => i.IsShared == false)
+                .First();
+            double projectPointElevation = projectBasePoint.get_BoundingBox(null).Min.Z;
+
+            double pointZ = point.Z;
+            List<Level> levels = new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .WhereElementIsNotElementType()
+                .Cast<Level>()
+                .ToList();
+
+            Level finalLevel = null;
+
+            foreach (Level lev in levels)
+            {
+                if (finalLevel == null)
+                {
+                    finalLevel = lev;
+                    continue;
+                }
+                if (lev.Elevation < finalLevel.Elevation)
+                {
+                    finalLevel = lev;
+                    continue;
+                }
+            }
+
+            double offset = 10000;
+            foreach (Level lev in levels)
+            {
+                double levHeigth = lev.Elevation + projectPointElevation;
+                double testElev = pointZ - levHeigth;
+                if (testElev < 0) continue;
+
+                if (testElev < offset)
+                {
+                    finalLevel = lev;
+                    offset = testElev;
+                }
+            }
+
+            return finalLevel.Id;
+        }
+
+    }
+}
